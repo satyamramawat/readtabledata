0dcead23d60bbbb196ab9692089b045a04abbd62
Satyam Ramawat <satyamramawat@gmail.com>
2018-12-16 19:20:41 +0530

    Removed commented functions

--- src/readWeb/readWeb.java
+++ src/readWeb/readWeb.java
@@ -3,12 +3,10 @@ package readWeb;
 
 import java.io.IOException;
 import java.util.List;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import com.giaybac.traprange.PDFTableExtractor;
 import com.giaybac.traprange.entity.Table;
 import java.io.File;
@@ -26,8 +24,8 @@ public class readWeb {
 
 			Elements tableHeaderEles = tableElements.select("thead tr th");
 			System.out.println("headers");
-			for (int i = 0; i < tableHeaderEles.size(); i++) {
-				System.out.println(tableHeaderEles.get(i).text());
+			for (int i = 0; i < tableHeaderEles.size(); i++) {                 
+				System.out.println(tableHeaderEles.get(i).text());  // This loop will read Row-by-Row
 			}
 			System.out.println();
 
@@ -35,10 +33,10 @@ public class readWeb {
 
 			for (int i = 0; i < tableRowElements.size(); i++) {
 				Element row = tableRowElements.get(i);
-				System.out.println("row");
+				System.out.println("row"+(i+1));
 				Elements rowItems = row.select("td");
-				for (int j = 0; j < rowItems.size(); j++) {
-					System.out.println(rowItems.get(j).text());
+				for (int j = 0; j < rowItems.size(); j++) {        // This loop will read elements of a selected row One-by-One
+					System.out.println(rowItems.get(j).text());    //rowItems.get(j).text() we can store this in a nth position of Array & ArrayList 
 				}
 				System.out.println();
 			}
@@ -47,25 +45,9 @@ public class readWeb {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	/*public void readpdfdata() throws IOException {
-		PDFTableExtractor extractor = new PDFTableExtractor();
-		List<Table> tables = extractor.setSource("users/satyamramawat/desktop/sample-1.pdf")
-			.addPage(0)
-			.addPage(1)
-			.exceptLine(0) //the first line in each page
-			.exceptLine(1) //the second line in each page
-			.exceptLine(-1)//the last line in each page
-			.extract();
-		String html = tables.get(0).toHtml();//table in html format
-		String csv = tables.get(0).toString();//table in csv format using semicolon as a delimiter 
-	}*/
-
 	public static void main(String[] args) throws IOException {
 		String html = "https://accessibility.umn.edu/web-designers/tables-web-pages";
 		readWeb parser = new readWeb();
 		parser.readhtmldata(html);
-		//parser.readpdfdata();
 	}
 }
